67dc7f62236ea7683a58e531af31408378c3c9f1
Rasesh Patel <raspat1@gmail.com>
2020-06-16 01:02:52 -0700

    Add Google analytics id.
    It's okay to hardcode this. It's client visible anyways. It'll save the
    time and additional comlplexity to get the environemnt variables set up.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: `G-SH9DHHL11Y`,
       },
     },
     {
